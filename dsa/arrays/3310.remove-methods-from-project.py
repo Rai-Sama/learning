0d6cb4647735d26cf17ab9c8428967d93ca42450
Anshuman Rai <anshumancos3@gmail.com>
2026-08-05 21:00:25 +0530

    chore: auto-sync mintbtw 2026-08-05 21:00

--- dsa/arrays/3310.remove-methods-from-project.py
+++ dsa/arrays/3310.remove-methods-from-project.py
@@ -6,58 +6,35 @@ from leetgo_py import *
 
 # @lc code=begin
 
-
 class Solution:
-    def remainingMethods(
-        self, n: int, k: int, invocations: List[List[int]]
-    ) -> List[int]:
+    def remainingMethods(self, n: int, k: int, invocations: List[List[int]]) -> List[int]:
         # Move forward -- add to dicts (K -> V), if K becomes suspicious (set) all V become suspicious
         # Remove suspicious set from set of all nodes
-        # If anything remains check if any of its dict V's are suspicious
+        # If anything remains, check if any of its dict V's are suspicious
         # If not, return them. If true for any one, return set of all nodes
         # If nothing remained just return the empty List
-        nodes = set()
+        nodes = [x for x in range(n)]
         sus = {k}
-        rels = {}
-        good_nodes = {}
+        rels = [[] for x in nodes]
         for i in invocations:
-            nodes.update([i[0], i[1]])  # Add new nodes to master set of nodes
-            if i[0] in rels:
-                rels[i[0]].add(i[1])  # Add edge
-            else:
-                rels[i[0]] = {i[1]}  # Edge from a new node
-            if i[0] in sus:
-                sus.add(i[1])  # Incoming node is sus so this node is sus
-                if i[1] in rels:
-                    sus.update(
-                        rels[i[1]]
-                    )  # All edges outgoing from sus node become sus
+            rels[i[0]].append(i[1])
 
-        print("Master list of nodes: ", nodes)
-        print("Sus nodes found: ", sus)
-
+        check = [x for x in sus]
+        while check:
+            x = check.pop()
+            for y in rels[x]:
+                if not y in sus:
+                    sus.add(y)
+                    check.append(y)
 
         # remove sus nodes from master set
-        remaining = nodes - sus
-        print("Remaining unsus nodes: ", remaining)
+        remaining = [x for x in nodes if not x in sus]
 
         for x in remaining:
-            if x in rels:
-                good_nodes = sus - rels[x]
-            else:
-                good_nodes = sus
-            if len(good_nodes) < len(
-                sus
-            ):  # Non-suspicious link goes to a sus one - removal impossible scenario
-                print("Bad nodes found: ", sus - good_nodes)
-                sol = list(nodes)
-                print("Returning: ", sol)
-                print("Should be the list version of: ", nodes)
-                return sol
-        print("Remaining nodes: ", remaining)
-        sol = list(remaining)
-        return sol
-
+            for y in rels[x]:
+                if y in sus:
+                    return nodes
+        return remaining
 
 # @lc code=end
 
@@ -66,3 +43,4 @@ if __name__ == "__main__":
     k: int = deserialize("int", read_line())
     invocations: List[List[int]] = deserialize("List[List[int]]", read_line())
     ans = Solution().remainingMethods(n, k, invocations)
+    print("\noutput:", serialize(ans, "integer[]"))
--- dsa/leetcode_sols/3310.remove-methods-from-project/solution.py
+++ dsa/leetcode_sols/3310.remove-methods-from-project/solution.py
@@ -1,4 +1,4 @@
-# Anshuman -- TODO_PATH_1
+# Anshuman -- arrays
 
 from typing import *
 
@@ -6,68 +6,35 @@ from leetgo_py import *
 
 # @lc code=begin
 
-def update_sus(curr, sus, rels): # Add all direct and indirect children of sus node as sus
-    if curr in sus:
-        if curr in rels:
-            sus.update(rels[curr])
-            for x in rels[curr]:
-                update_sus(x, sus, rels)
-        else:
-            return sus
-    else:
-        return sus
-
 class Solution:
     def remainingMethods(self, n: int, k: int, invocations: List[List[int]]) -> List[int]:
         # Move forward -- add to dicts (K -> V), if K becomes suspicious (set) all V become suspicious
         # Remove suspicious set from set of all nodes
-        # If anything remains check if any of its dict V's are suspicious
+        # If anything remains, check if any of its dict V's are suspicious
         # If not, return them. If true for any one, return set of all nodes
         # If nothing remained just return the empty List
-        nodes = {x for x in range(n)}
+        nodes = [x for x in range(n)]
         sus = {k}
-        rels = {}
-        good_nodes = {}
-        print("Nodes: ", nodes)
+        rels = [[] for x in nodes]
         for i in invocations:
-            if i[0] in rels:
-                rels[i[0]].add(i[1])  # Add edge
-            else:
-                rels[i[0]] = {i[1]}  # Edge from a new node
-            if i[0] in sus:
-                sus.add(i[1])  # Incoming node is sus so this node is sus
-                if i[1] in rels:
-                    sus.update(rels[i[1]])  # All edges outgoing from sus node become sus           
-        
-        for x in sus.copy():
-            if x in rels:
-                for y in rels[x]:
-                    if not y in sus:
-                        sus.add(y)
+            rels[i[0]].append(i[1])
 
-        print("Relationships: ", rels)
-        print()
-        print("Sus nodes: ", sus)
+        check = [x for x in sus]
+        while check:
+            x = check.pop()
+            for y in rels[x]:
+                if not y in sus:
+                    sus.add(y)
+                    check.append(y)
 
         # remove sus nodes from master set
-        remaining = nodes - sus
-        print("Unsus nodes: ", remaining)
+        remaining = [x for x in nodes if not x in sus]
 
         for x in remaining:
-            if x in rels:
-                good_nodes = sus - rels[x]
-            else:
-                good_nodes = sus
-            if len(good_nodes) < len(
-                sus
-            ):  # Non-suspicious link goes to a sus one - removal impossible scenario
-                sol = list(nodes)
-                print("Found a bad node when checking unsus node: ", x)
-                print("Children of x:", rels.get(x, {}))
-                print("Found a bad node (not in sus but had a sus child): ", sus - good_nodes)
-                return sol
-        sol = list(remaining)
-        return sol
+            for y in rels[x]:
+                if y in sus:
+                    return nodes
+        return remaining
 
 # @lc code=end
 
